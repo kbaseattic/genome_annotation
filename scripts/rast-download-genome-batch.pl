9552c5bdc7825c4204d197879d071bcd338289d2
Robert Olson <olson@mcs.anl.gov>
2014-07-03 15:29:36 -0500

    Finish query, add download.

---
+++ scripts/rast-download-genome-batch.pl
@@ -0,0 +1,79 @@
+
+use strict;
+use Getopt::Long::Descriptive;
+use Data::Dumper;
+use Bio::KBase::GenomeAnnotation::CmdHelper qw(:all);
+use Bio::KBase::HandleService;
+use JSON::XS;
+
+=head1 NAME
+
+rast-download-genome-batch
+
+=head1 SYNOPSIS
+
+rast-download-genome-batch batch-id
+    
+=head1 DESCRIPTION
+    
+=head1 COMMAND-LINE OPTIONS
+
+rast-process-genome-batch.pl [-h] [long options...] directory-of-genome-objects
+    --workflow      File containing genome processing workflow
+                    specification
+    -h --help       print usage message and exit
+    --url           URL for the genome annotation service
+    
+=cut
+
+my @options = (options_help());
+
+our($opt, $usage) = describe_options("%c %o batch-id output-directory",
+				    ["verbose|v", "Verbose output"],
+				    @options);
+
+print($usage->text), exit if $opt->help;
+print($usage->text), exit 1 if @ARGV != 2;
+
+my $batch_id = shift;
+my $dir = shift;
+
+-d $dir or die "Output directory $dir does not exist\n";
+
+my $client = get_annotation_client($opt);
+my $hservice = Bio::KBase::HandleService->new();
+
+my $status = $client->pipeline_batch_status($batch_id);
+
+for my $ent (@$status)
+{
+    my $gid = $ent->{genome_id};
+    if ($ent->{status} ne 'completed')
+    {
+	print STDERR "Skipping $gid: not complete (status is $ent->{status})\n" if $opt->verbose;
+	next;
+    }
+    
+    save($ent->{stdout}, "$dir/$gid.stdout");
+    save($ent->{stderr}, "$dir/$gid.stderr");
+    save($ent->{output}, "$dir/$gid.gto");
+}
+
+sub save
+{
+    my($h, $file) = @_;
+    my $url = get_url($h);
+
+    print "Save $url to $file\n" if $opt->verbose;
+    unlink($file);
+    $hservice->download($h, $file);
+}
+
+sub get_url
+{
+    my($h) = @_;
+    return "$h->{url}/node/$h->{id}?download";
+}
+     
+
+
